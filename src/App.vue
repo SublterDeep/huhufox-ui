03b689c01155f7bbdcf0cb31a158e12d7c11b3bc
SublterDeep <sublterdeep@163.com>
2024-07-08 14:23:39 +0800

    上传至GitHub

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <fox_slider :arrData="arr_1"></fox_slider>
+  </div>
+</template>
+
+<script>
+import fox_slider from '../plugins/components/slider/fox_slider.vue'
+
+export default {
+  name: 'App',
+  components: {
+    fox_slider
+  },
+  data() {
+    return {
+      arr_1: ['01', '02', '03', '04'],
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TestComponents.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="TestComponents">
+    TestComponents
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponents',
+}
+</script>
+<style scoped>
+</style>
